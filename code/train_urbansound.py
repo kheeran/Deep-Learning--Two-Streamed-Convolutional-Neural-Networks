812471957e5f7eed159b0c730a58da2a1205e37f
Kheeran <kn16063@bristol.ac.uk>
2019-11-29 19:17:25 +0000

    CNN class - fully specified forward pass

--- code/train_urbansound.py
+++ code/train_urbansound.py
@@ -19,7 +19,7 @@ torch.backends.cudnn.benchmark = True
 # import argparse
 # from pathlib import Path
 
-class DataShape(NamedTuple):
+class SoundShape(NamedTuple):
     height: int
     width: int
     channels_xxx: int
@@ -36,7 +36,7 @@ else:
 class CNN(nn.Module):
     def __init__(self, height: int, width: int, channels_xxx:int, class_count: int, dropout: float):
         super().__init__()
-        self.input_shape = DataShape(height=height, width=width)
+        self.input_shape = SoundShape(height=height, width=width)
         self.class_count = class_count
 
         self.conv1 = nn.Conv2d(
@@ -45,9 +45,7 @@ class CNN(nn.Module):
             kernel_size=(3,3),
             padding=(1,1),
         )
-
         self.initialise_layer(self.conv1)
-
         self.bnorm1 = nn.BatchNorm2d(
             num_features=32
         )
@@ -58,13 +56,11 @@ class CNN(nn.Module):
             kernel_size=(3,3),
             padding=(1,1),
         )
-
         self.initialise_layer(self.conv2)
 
         self.bnorm2 = nn.BatchNorm2d(
             num_features=64
         )
-
         # Pooling Layer with stride to half the output
         self.pool2 = nn.MaxPool2d(
             kernel_size=(2,2),
@@ -77,7 +73,6 @@ class CNN(nn.Module):
             kernel_size=(3,3),
             padding=(1,1),
         )
-
         self.bnorm3 = nn.BatchNorm2d(
             num_features=64
         )
@@ -90,23 +85,57 @@ class CNN(nn.Module):
             padding=(1,1),
             stride=(2,2),
         )
-
         self.bnorm4 = nn.BatchNorm2d(
             num_features=64
         )
 
         self.fc1 = nn.Linear(15488, 1024)
         self.initialise_layer(self.fc1)
-
         self.bnormfc1 = nn.BatchNorm1d(
             num_features = 1024
         )
 
         self.fcout = nn.Linear (1024, 10)
-
         self.initialise_layer(self.fcout)
 
-    
+        self.dropout = nn.Dropout(p=dropout)
+
+    def forward(self, sounds: torch.Tensor) -> torch.Tensor:
+        # Hidden Layer 1
+        x = self.conv1(sounds)
+        x = self.bnorm1(x)
+        x = F.relu(x)
+
+        # Hidden Layer 2
+        x = self.conv2(self.dropout(x))
+        x = self.bnorm2(x)
+        x = F.relu(x)
+        x = self.pool2(x)
+
+        # Hidden Layer 3
+        x = self.conv3(x)
+        x = self.bnorm3(x)
+        x = F.relu(x)
+
+        # Hidden Layer 4
+        x = self.conv4(self.dropout(x))
+        x = self.bnorm4(x)
+        x = F.relu(x)
+
+        # Flatten Hidden Layer 4
+        x = torch.flatten(x, start_dim = 1)
+
+        # Fully Conected Layer 1 (do we put dropout on both FC layers?)
+        x = self.fc1(self.dropout(x))
+        x = self.bnormfc1(x) # This was not in the paper
+        x = F.sigmoid(x)
+
+        # Fully Conected Layer 2 (do we put dropout on both FC layers?)
+        x = self.fc2(self.dropout(x))
+        
+        return x
+
+
 
     # Initialise weights using Kaiming
     @staticmethod
