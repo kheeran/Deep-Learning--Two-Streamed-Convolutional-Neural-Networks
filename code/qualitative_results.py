08af8dd79cfe5b56c16be86fab8fd5a75b6995ac
Kheeran <kn16063@bristol.ac.uk>
2020-01-11 01:10:12 +0000

    getting information about the efficacy of the NN on eacy data point

--- code/qualitative_results.py
+++ code/qualitative_results.py
@@ -4,3 +4,29 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 from random import randint
+
+results_epoch_LMC = pickle.load(open("TSCNN_store_LMC_Final.pkl", "rb"))
+results_epoch_MC = pickle.load(open("TSCNN_store_MC_Final.pkl", "rb"))
+# results_epoch_TSCNN = pickle.load(open("TSCNN_store_TSCNN.pkl", "rb"))
+
+Final_LMC = results_epoch_LMC[49]
+Final_MC = results_epoch_MC[49]
+
+print(len(Final_LMC['preds']))
+
+# indices where LMC and MC both classify correctly
+LMC_vs_MC_correct = np.logical_and((np.array(Final_LMC['preds']) == np.array(Final_LMC["labels"])),(np.array(Final_MC['preds'] == np.array(Final_LMC["labels"]))))
+x = np.where(LMC_vs_MC_correct)[0]
+print(x.shape)
+
+# indices where LMC and MC both classify wrongly
+LMC_vs_MC_wrong = np.logical_and((np.array(Final_LMC['preds']) != np.array(Final_LMC["labels"])),(np.array(Final_MC['preds'] != np.array(Final_LMC["labels"]))))
+print((np.where(LMC_vs_MC_wrong))[0].shape)
+
+# indices where either LMC is correct and MC is wrong
+LMC_vs_MC_LMC = np.logical_and((np.array(Final_LMC['preds']) == np.array(Final_LMC["labels"])),(np.array(Final_MC['preds'] != np.array(Final_LMC["labels"]))))
+print((np.where(LMC_vs_MC_LMC))[0].shape)
+
+# indices where either LMC is wrong and MC is correct
+LMC_vs_MC_MC = np.logical_and((np.array(Final_LMC['preds']) != np.array(Final_LMC["labels"])),(np.array(Final_MC['preds'] == np.array(Final_LMC["labels"]))))
+print((np.where(LMC_vs_MC_MC))[0].shape)
