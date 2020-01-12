656f5d106b61994118fb45db560a005e2871c63b
Kheeran <kn16063@bristol.ac.uk>
2020-01-12 15:37:05 +0000

    finding the indices and fnames of the segments qualitatively

--- code/dataset.py
+++ code/dataset.py
@@ -13,6 +13,8 @@ class UrbanSound8KDataset(data.Dataset):
     def __getitem__(self, index):
 
         dataset = np.array(self.dataset)
+        label = self.dataset[index]['classID']
+        fname = self.dataset[index]['filename']
 
         LM = dataset[index]["features"]["logmelspec"]
         MFCC = dataset[index]["features"]["mfcc"]
@@ -35,6 +37,8 @@ class UrbanSound8KDataset(data.Dataset):
             plt.imshow(T)
             plt.show()
             feature = torch.from_numpy(LMC.astype(np.float32)).unsqueeze(0)
+            print(fname)
+            print(label)
         elif self.mode == 'MC':
             # Edit here to load and concatenate the neccessary features to
             # create the MC feature
@@ -46,14 +50,14 @@ class UrbanSound8KDataset(data.Dataset):
             MLMC = np.concatenate((MFCC, LM, C, SC, T), axis=0)
             feature = torch.from_numpy(MLMC.astype(np.float32)).unsqueeze(0)
 
-        label = self.dataset[index]['classID']
-        fname = self.dataset[index]['filename']
         return feature, label, fname
 
     def __len__(self):
         return len(self.dataset)
 
 dataset_train_LMC = UrbanSound8KDataset("./UrbanSound8K_test.pkl", "LMC")
+dataset_train_MC = UrbanSound8KDataset("./UrbanSound8K_test.pkl", "MC")
+
 
 # for i in range (0,20):
 #     a = randint(0,5395)
@@ -63,7 +67,17 @@ dataset_train_LMC = UrbanSound8KDataset("./UrbanSound8K_test.pkl", "LMC")
 #     plt.hlines(66, 0, 40)
 #     plt.hlines(71, 0, 40)
 #     plt.show()
-dataset_train_LMC.__getitem__(45)[0]
+
+# set = np.array([3064, 3065, (3066), (3067), (3068), (3069), (3070), 3071, 3072])
+#
+# set = np.array([(2244), (2245), (2246), (2247), (2248), (2249), (2250)])
+# for i in set:
+#     dataset_train_LMC.__getitem__(i)[0]
+
+i = 4690
+dataset_train_LMC.__getitem__(i)[0]
+
+
 # labels = np.zeros((10,1))
 # print(labels)
 # for i in range(5395):
--- code/qualitative_results.py
+++ code/qualitative_results.py
@@ -19,19 +19,39 @@ print(len(Final_LMC['preds']))
 LMC_vs_MC_correct = np.logical_and((np.array(Final_LMC['preds']) == np.array(Final_LMC["labels"])),(np.array(Final_MC['preds'] == np.array(Final_LMC["labels"]))))
 x = np.where(LMC_vs_MC_correct)[0]
 print(x.shape)
+print(x)
 
-# indices where either LMC is correct and MC is wrong
+# indices where LMC is correct and MC is wrong
 LMC_vs_MC_LMC = np.logical_and((np.array(Final_LMC['preds']) == np.array(Final_LMC["labels"])),(np.array(Final_MC['preds'] != np.array(Final_LMC["labels"]))))
 print((np.where(LMC_vs_MC_LMC))[0].shape)
+print((np.where(LMC_vs_MC_LMC))[0])
 
-# indices where either LMC is wrong and MC is correct
+# indices where LMC yes MC no but TSCNN yes
+TSCNN = np.logical_and(LMC_vs_MC_LMC, (Final_TSCNN['preds']) == np.array(Final_TSCNN["labels"]))
+print((np.where(TSCNN))[0].shape)
+print((np.where(TSCNN))[0])
+
+# indices where LMC is wrong and MC is correct
 LMC_vs_MC_MC = np.logical_and((np.array(Final_LMC['preds']) != np.array(Final_LMC["labels"])),(np.array(Final_MC['preds'] == np.array(Final_LMC["labels"]))))
 print((np.where(LMC_vs_MC_MC))[0].shape)
+print((np.where(LMC_vs_MC_MC))[0])
+
+# indices where LMC no MC yes but TSCNN yes
+TSCNN = np.logical_and(LMC_vs_MC_MC, (Final_TSCNN['preds']) == np.array(Final_TSCNN["labels"]))
+print((np.where(TSCNN))[0].shape)
+print((np.where(TSCNN))[0])
 
 # indices where LMC and MC both classify wrongly
 LMC_vs_MC_wrong = np.logical_and((np.array(Final_LMC['preds']) != np.array(Final_LMC["labels"])),(np.array(Final_MC['preds'] != np.array(Final_LMC["labels"]))))
 print((np.where(LMC_vs_MC_wrong))[0].shape)
+print((np.where(LMC_vs_MC_wrong))[0])
 
 # indices where LMC and MC both classify wrongly but TSCNN is correct
-LMC_vs_MC_vs_TSCNN = np.logical_and(LMC_vs_MC_wrong, Final_TSCNN['preds']) == np.array(Final_TSCNN["labels"])
+LMC_vs_MC_vs_TSCNN = np.logical_and(LMC_vs_MC_wrong, (Final_TSCNN['preds']) == np.array(Final_TSCNN["labels"]))
 print((np.where(LMC_vs_MC_vs_TSCNN))[0].shape)
+print((np.where(LMC_vs_MC_vs_TSCNN))[0])
+
+# indices where LMC and MC and TSCNN are all wrong
+LMC_vs_MC_vs_TSCNN_wrong = np.logical_and(LMC_vs_MC_wrong, (Final_TSCNN['preds']) != np.array(Final_TSCNN["labels"]))
+print((np.where(LMC_vs_MC_vs_TSCNN_wrong))[0].shape)
+print((np.where(LMC_vs_MC_vs_TSCNN_wrong))[0])
